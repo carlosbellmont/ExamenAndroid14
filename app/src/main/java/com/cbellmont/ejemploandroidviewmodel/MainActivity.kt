15b914a30075bdcaf8dd2a98738d6476768fa320
Carlos de Tena Bellmont <carlos.bellmont@gmail.com>
2020-08-17 10:51:47 +0200

    Coroutines

--- app/src/main/java/com/cbellmont/ejemploandroidviewmodel/MainActivity.kt
+++ app/src/main/java/com/cbellmont/ejemploandroidviewmodel/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.cbellmont.ejemploandroidviewmodel
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var model :MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val model = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        model = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        downloadAll()
 
-        model.downloadFilms()
 
-        model.films.forEach {
-            tvFilms.append("${it.name}\n")
+    }
+
+    private fun downloadAll(){
+        CoroutineScope(IO).launch {
+            val list = model.getFilms()
+            setTextOnMainThread(list)
         }
+    }
 
+    private suspend fun setTextOnMainThread(list: MutableList<Film>) {
+        withContext (Main) {
+            list.forEach {
+                tvFilms.append("${it.name}\n")
+            }
+            pbLoading.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/com/cbellmont/ejemploandroidviewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/cbellmont/ejemploandroidviewmodel/MainActivityViewModel.kt
@@ -1,24 +1,44 @@
 package com.cbellmont.ejemploandroidviewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivityViewModel  : ViewModel() {
 
-    val films = mutableListOf<Film>()
+    private val films = mutableListOf<Film>()
 
-    fun downloadFilms() {
-        films.addAll(mutableListOf(
-            Film(1, "La Amenaza Fantasma", "aaaa"),
-            Film(2, "El Ataque de los Clones", "aaaa"),
-            Film(3, "La Venganza de los Sith", "aaaa"),
-            Film(4, "Una Nueva Esperanza", "aaaa"),
-            Film(5, "El Imperio Contraataca", "aaaa"),
-            Film(6, "El Retorno del Jedi", "aaaa"),
-            Film(4, "El Despertar de la Fuerza", "aaaa"),
-            Film(5, "Los Útimos Jedi", "aaaa"),
-            Film(6, "El Ascenso de Skywalker", "aaaa")
-        ))
+    suspend fun getFilms(): MutableList<Film> = withContext(Dispatchers.IO)  {
+        delay(5000)
+        if (films.isEmpty()){
+            downloadFilms()
+        }
+        return@withContext films
     }
+
+    private fun downloadFilms() {
+        viewModelScope.launch {
+            films.addAll(
+                mutableListOf(
+                    Film(1, "La Amenaza Fantasma", "aaaa"),
+                    Film(2, "El Ataque de los Clones", "aaaa"),
+                    Film(3, "La Venganza de los Sith", "aaaa"),
+                    Film(4, "Una Nueva Esperanza", "aaaa"),
+                    Film(5, "El Imperio Contraataca", "aaaa"),
+                    Film(6, "El Retorno del Jedi", "aaaa"),
+                    Film(4, "El Despertar de la Fuerza", "aaaa"),
+                    Film(5, "Los Útimos Jedi", "aaaa"),
+                    Film(6, "El Ascenso de Skywalker", "aaaa")
+                )
+            )
+
+        }
+    }
+
+
 
 }
